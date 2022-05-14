e10259c2e4045df7b2abb15313415539c6c2c2c7
Tiago Barracha <taigo.barracha@gmail.com>
2022-05-14 20:24:34 +0100

    Clean Up

--- Assets/Plugins/Stardrop Tools/Scripts/Library/Loop Manager/LoopManagerInitializer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using UnityEngine;
-
-
-public static class LoopManagerInitializer
-{
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-    public static void Initializer() => LoopManager.Instance.Initialize();
-}
